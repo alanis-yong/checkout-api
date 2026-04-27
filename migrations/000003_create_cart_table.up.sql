9cbd52c06bdb4a801eb9d3162c2ad6314366e7ef
Alanis Yong <alanisyongyr@gmail.com>
2026-04-28 02:36:32 +0800

    fixed xsolla payload

--- migrations/000003_create_cart_table.up.sql
+++ migrations/000003_create_cart_table.up.sql
@@ -1,8 +1,7 @@
-BEGIN;
-CREATE TABLE IF NOT EXISTS cart_items (
+CREATE TABLE IF NOT EXISTS cart (
+  id SERIAL PRIMARY KEY,
   user_id TEXT NOT NULL,
   sku TEXT NOT NULL,
-  quantity INTEGER DEFAULT 1,
-  PRIMARY KEY (user_id, sku)
+  quantity INTEGER NOT NULL DEFAULT 1,
+  UNIQUE(user_id, sku)
 );
-COMMIT;
